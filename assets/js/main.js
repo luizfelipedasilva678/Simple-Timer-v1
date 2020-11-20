4ffd711691e834fbf9135c189db0dc33c0929d68
Luiz Felipe <luizfelipedasilva678@gmail.com>
2020-11-19 21:31:37 -0300

    Adding functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@
     let seconds = document.querySelector('.seconds');
     let minutes = document.querySelector('.minutes');
     let hours = document.querySelector('.hours');
+    var interval;
 
     function Timer(h = 0, m = 0, s = 0) {
         let hour = h;
@@ -12,7 +13,7 @@
         minutes.innerHTML = addLeftZero(minute);
         hours.innerHTML = addLeftZero(hour);
 
-        let interval = setInterval(() => {
+        interval = setInterval(() => {
             second++;
             seconds.innerHTML = addLeftZero(second);
             
@@ -41,29 +42,29 @@
             }
         }, 1000)
 
-        document.querySelector(".stop").addEventListener('click', () => {
-            clearInterval(interval);
-            let valueSecond = Number(document.querySelector('.seconds').innerText);
-            let valueMinute = Number(document.querySelector('.minutes').innerText);
-            let valueHour = Number(document.querySelector('.hours').innerText);
-            document.querySelector('h2').style.color = "red";
-
-            document.querySelector('.resume').addEventListener('click', () => {
-                Timer(valueHour, valueMinute, valueSecond);
-                document.querySelector('h2').style.color = "#000";
-            })
-            
-        }); 
-        
-        document.querySelector(".restart").addEventListener('click', () => {
-            clearInterval(interval);
-            seconds.innerHTML = addLeftZero(0);
-            minutes.innerHTML = addLeftZero(0);
-            hours.innerHTML = addLeftZero(0);
-
-            Timer();
-        });     
     }   
+    
+    function stop(){
+        clearInterval(interval);
+        let valueSecond = Number(document.querySelector('.seconds').innerText);
+        let valueMinute = Number(document.querySelector('.minutes').innerText);
+        let valueHour = Number(document.querySelector('.hours').innerText);
+        document.querySelector('h2').style.color = "red";
+
+        document.querySelector('.resume').addEventListener('click', () => {
+            Timer(valueHour, valueMinute, valueSecond);
+            document.querySelector('h2').style.color = "#000";
+        })   
+    }; 
+    
+    function restart() {
+        clearInterval(interval);
+        seconds.innerHTML = addLeftZero(0);
+        minutes.innerHTML = addLeftZero(0);
+        hours.innerHTML = addLeftZero(0);
+
+        Timer();
+    };
 
     function addLeftZero(number) {
         return number < 10 ? '0' + number : number;
@@ -74,5 +75,13 @@
         if( e.target.className === 'init' ) {
             Timer();
         } 
+
+        if( e.target.className === 'stop' ) {
+            stop();
+        }
+
+        if( e.target.className === 'restart' ) {
+            restart();
+        }
     });
 })();
